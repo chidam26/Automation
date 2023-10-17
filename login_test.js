2d0a113bf50b31d3fa066b404c786f76213d37e1
chidam26 <89583061+chidam26@users.noreply.github.com>
2023-10-17 17:47:56 +0530

    playwright-form-text

---
+++ login_test.js
@@ -0,0 +1,21 @@
+const { I } = inject();
+Feature('login');
+
+Scenario('test something', async ({ I }) => {
+  I.amOnPage('https://demoqa.com/text-box');
+  I.seeElement("//label[@id='userName-label']");
+  I.fillField('#userName', 'Chidambaram Natarajan');
+  I.seeElement('#userEmail-label')
+  I.fillField('#userEmail', 'xyz@gmail.com');
+  I.seeElement('#currentAddress-label')
+  I.fillField('Current Address', 'Trichy');
+  I.seeElement('#permanentAddress-label');
+  I.fillField('#permanentAddress', 'Trichy');
+  I.click('Submit');
+  I.see('Chidambaram Natarajan', '#name');
+  I.see('xyz@gmail.com', '#email');
+  I.see('Trichy', '#currentAddress');
+  I.see('Trichy', '#permanentAddress');
+  pause();
+});
+
