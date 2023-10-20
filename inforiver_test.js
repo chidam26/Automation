36a20e0a4961d5847d28c1f6ecdd29e48791103b
chidam26 <89583061+chidam26@users.noreply.github.com>
2023-10-20 20:34:28 +0530

    notes column adding scenario

--- inforiver_test.js
+++ inforiver_test.js
@@ -56,7 +56,7 @@ async function checkFontStyleApplied(targetRegion, fontStyle) {
     await assert.equal(fontStyle, cleanedFont, "Font Style doesn't match")
 }
 
-Scenario ('apply formattings', async ({ I }) => {
+Scenario.skip ('apply formattings', async ({ I }) => {
     await I.amOnPage('https://inforiverwebtest-dev.azurewebsites.net/?csvLocation=https://sabareesh-r23.github.io/MatrixCsvAndConfig/Sanity.csv&config=https://sabareesh-r23.github.io/MatrixCsvAndConfig/Sanity.json&URLLoad=true');
     await I.wait(5)
     const targetRegion = 'USA';
@@ -68,4 +68,25 @@ Scenario ('apply formattings', async ({ I }) => {
     let fontWeight = "16";
     await selectFontWeight(fontWeight);
     await checkFontStyleApplied(targetRegion, fontStyle);
+})
+
+Scenario ('notes column', async ({ I }) => {
+    await I.amOnPage('https://inforiverwebtest-dev.azurewebsites.net/?csvLocation=https://sabareesh-r23.github.io/MatrixCsvAndConfig/Sanity.csv&config=https://sabareesh-r23.github.io/MatrixCsvAndConfig/Sanity.json&URLLoad=true');
+    await I.wait(5);
+    await I.click(`//div[@class='toolbar_columnMenu']`);
+    await I.click('Notes Column');
+    const targetRegion = "USA"
+    const targetRegionXpath = `//div[@class='non-sticky-grid-cells']//span[text()="${targetRegion}"]/ancestor::div[5]`;
+    const targetRegionXpathId = await I.grabAttributeFrom(targetRegionXpath, 'id')
+    const notesDiv = `(//div[@class='title-label'])[7]/ancestor::div[1]`
+    const notesDivClass = await I.grabAttributeFrom(notesDiv, 'class')
+    const [rowNumber, colNumber] = targetRegionXpathId.match(/\d+/g);
+    const colNumberFromClass = notesDivClass.match(/\d+/g);
+    const result = `table-row-${rowNumber}_table-col-${colNumberFromClass}`;
+    await I.doubleClick(`//div[@id='${result}']`);
+    await I.click(`.DraftEditor-editorContainer`);
+    await I.pressKey(`U`);
+    await I.click('Save');
+    await I.see('U', `//div[@id='table-row-2_table-col-17']/div/div/div/p`);
+    pause()
 })
